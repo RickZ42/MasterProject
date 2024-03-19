50095f395c4ef356a45bf6be94b0585199f3a8b1
RickZ42 <u7457512@anu.edu.au>
2024-03-19 23:31:14 +1100

    modified dorado script

--- basecalling278.sh
+++ basecalling278.sh
@@ -8,7 +8,7 @@
 #PBS -l ncpus=24
 #PBS -l mem=96GB
 #PBS -l jobfs=400GB
-#PBS -l storage=scratch/xf3+gdata/xf3
+#PBS -l storage=scratch/xf3+gdata/xf3+gdata/xe2
 #PBS -l wd
 
 set -xue
@@ -18,8 +18,8 @@ DORADO_PATH="/g/data/xf3/rick/envs/dorado-0.5.3-linux-x64/bin/dorado"
 
 # Set the input directories
 INPUT_DIRS=(
-    "/g/data/xe2/datasets/uploads_public/ctrap_ont_pod5/20231017_1728_1F_PAQ09278_af45b1bf/pod5_pass"
-    "/g/data/xe2/datasets/uploads_public/ctrap_ont_pod5/20231023_1117_1F_PAQ09278_674d2cfd/pod5_pass"
+    "/g/data/xe2/datasets/uploads_public/ctrap_ont_pod5/20231017_1728_1F_PAQ09278_af45b1bf/pod5_pass/"
+    "/g/data/xe2/datasets/uploads_public/ctrap_ont_pod5/20231023_1117_1F_PAQ09278_674d2cfd/pod5_pass/"
 )
 
 # Set the output file
@@ -29,7 +29,7 @@ OUTPUT_FILE="/g/data/xf3/rick/Output/DoradoSup278.bam"
 # Iterate over each path
 for DIRS in "${INPUT_DIRS[@]}"; do
 
-"${DORADO_PATH}" basecaller sup "${DIRS}" > "${OUTPUT_FILE}"
+"${DORADO_PATH}" basecaller /g/data/xf3/rick/script/dna_r10.4.1_e8.2_400bps_sup@v4.3.0 "${DIRS}" >> "${OUTPUT_FILE}"
 
 done
 
--- basecalling765.sh
+++ basecalling765.sh
@@ -8,7 +8,7 @@
 #PBS -l ncpus=24
 #PBS -l mem=96GB
 #PBS -l jobfs=400GB
-#PBS -l storage=scratch/xf3+gdata/xf3
+#PBS -l storage=scratch/xf3+gdata/xf3+gdata/xe2
 
 set -xue
 
@@ -17,8 +17,8 @@ DORADO_PATH="/g/data/xf3/rick/envs/dorado-0.5.3-linux-x64/bin/dorado"
 
 # Set the input directories
 INPUT_DIRS=(
-    "/g/data/xe2/datasets/uploads_public/ctrap_ont_pod5/20231017_1600_1C_PAQ11765_257f28d4/pod5_pass"
-    "/g/data/xe2/datasets/uploads_public/ctrap_ont_pod5/20231023_1117_1G_PAQ11765_bed6eab9/pod5_pass"
+    "/g/data/xe2/datasets/uploads_public/ctrap_ont_pod5/20231017_1600_1C_PAQ11765_257f28d4/pod5_pass/"
+    "/g/data/xe2/datasets/uploads_public/ctrap_ont_pod5/20231023_1117_1G_PAQ11765_bed6eab9/pod5_pass/"
 )
 
 # Set the output file
@@ -28,7 +28,7 @@ OUTPUT_FILE="/g/data/xf3/rick/Output/DoradoSup765.bam"
 # Iterate over each path
 for DIRS in "${INPUT_DIRS[@]}"; do
 
-"${DORADO_PATH}" basecaller sup "${DIRS}" > "${OUTPUT_FILE}"
+"${DORADO_PATH}" basecaller /g/data/xf3/rick/script/dna_r10.4.1_e8.2_400bps_sup@v4.3.0 "${DIRS}" >> "${OUTPUT_FILE}"
 
 done
 # Keep the job running indefinitely (optional)
