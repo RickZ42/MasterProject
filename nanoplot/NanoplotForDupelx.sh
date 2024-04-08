c4c358b615b9ab80124a5482c4e45453dbfcd740
RickZ42 <u7457512@anu.edu.au>
2024-04-08 19:30:09 +1000

    aaa

--- nanoplot/NanoplotForDupelx.sh
+++ nanoplot/NanoplotForDupelx.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#PBS -N NanoplotForQ720KONT
+#PBS -N NanoplotForDuplex
 #PBS -P xf3
 #PBS -q normal
 #PBS -l walltime=24:00:00
-#PBS -l ncpus=24
+#PBS -l ncpus=1
 #PBS -l mem=96GB
 #PBS -l jobfs=400GB
 #PBS -l storage=scratch/xf3+gdata/xf3+gdata/xe2
@@ -14,18 +14,18 @@ source /g/data/xf3/miniconda/etc/profile.d/conda.sh
 conda activate /g/data/xf3/miniconda/envs/nanoplot
 
 # Set the path to the input BAM file
-INPUT_file=("/g/data/xf3/zz3507/Output/chopper_output/Q720K/MergedONTQ720K/mergedONTQ720K.fq.gz")
+INPUT_file=("/g/data/xf3/zz3507/Output/fastqduplex/DoradoSup278Duplex_dx:1_only.fq.gz" "/g/data/xf3/zz3507/Output/fastqduplex/DoradoSup765Duplex_dx:1_only.fq.gz")
 
 # the output directory for NanoPlot results will be in the same path with input
 
 # Run NanoPlot
 for file in "${INPUT_file[@]}"
 do
-    NanoPlot -t 24 \
-         --fasta "$file" \
+    NanoPlot -t1 \
+         --fastq "$file" \
          --color yellow \
          --N50 \
          --prefix "${file%.*}" \
-         -o /g/data/xf3/zz3507/Output/chopper_output/Q720K/MergedONTQ720K
+         -o /g/data/xf3/zz3507/Output/fastqduplex
 
 done
