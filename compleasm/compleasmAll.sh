8e4c47fc09808643e319aa6e0109309cf8750cd7
RickZ42 <u7457512@anu.edu.au>
2024-04-08 11:53:08 +1000

    start ONT+UL+HiC assembly

--- compleasm/compleasmAll.sh
+++ compleasm/compleasmAll.sh
@@ -15,7 +15,7 @@ conda activate /g/data/xf3/miniconda/envs/compleasm
 
 input_file="/scratch/xf3/zz3507/result/HiFisam/AssembleResultV1fasta/Ctrap_HiFI_HiC_ONT30KB.hic.p_ctg.fasta"
 output_dir="/g/data/xf3/zz3507/Output/assemblyV1/compleasm/hap2"
-lineage_dir="/g/data/xf3/zz3507/script/compleasm/lineage"
+lineage_dir="/g/data/xf3/zz3507/database/lineage"
 threads=24
 
 compleasm run -a "$input_file" -o "$output_dir" -l embryophyta_odb10 -t "$threads" -L "$lineage_dir"
