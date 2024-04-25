6ba18b26a4a8e7870ce9d26d0f81e708e69e733f
RickZ42 <u7457512@anu.edu.au>
2024-04-26 09:51:14 +1000

    after running the h2 3Ddna

---
+++ BWA/BwaIndexH1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#PBS -N Bwah1
+#PBS -P xf3
+#PBS -q normal
+#PBS -l walltime=24:00:00
+#PBS -l ncpus=24
+#PBS -l mem=96GB
+#PBS -l jobfs=400GB
+#PBS -l storage=scratch/xf3+gdata/xf3+gdata/xe2
+
+set -xue
+
+
+/g/data/xf3/zz3507/envs/BWA/bwa/bwa index /g/data/xf3/zz3507/Output/assemblyQ720KOntHifiHiC/assemblyQ720KOntHifiHiCBiggerThan50KContig/assemblyQ720KOntHifiHiCBigger50Kcontig.hic.hap1_ctg.fasta
+
+
