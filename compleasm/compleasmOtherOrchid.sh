efe253bce61e8c4b5690c852d1f62422b0eb1c6b
RickZ42 <u7457512@anu.edu.au>
2024-05-02 20:37:44 +1000

    juicer successly run

---
+++ compleasm/compleasmOtherOrchid.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#PBS -N compleasmOtherOrchid
+#PBS -P xf3
+#PBS -q normal
+#PBS -l walltime=4:00:00
+#PBS -l ncpus=24
+#PBS -l mem=96GB
+#PBS -l jobfs=400GB
+#PBS -l storage=scratch/xf3+gdata/xf3+gdata/xe2
+#PBS -l wd
+
+source /g/data/xf3/miniconda/etc/profile.d/conda.sh
+conda activate /g/data/xf3/miniconda/envs/compleasm
+
+
+input_file="/g/data/xf3/zz3507/database/OtherOrchidGenome/shenzhenica/data/GCA_002786265.1/GCA_002786265.1_ASM278626v1_genomic.fna"
+output_dir="/g/data/xf3/zz3507/database/OtherOrchidGenome/shenzhenica/compleasm"
+lineage_dir="/g/data/xf3/zz3507/database/lineage"
+threads=24
+
+compleasm run -a "$input_file" -o "$output_dir" -l embryophyta_odb10 -t "$threads" -L "$lineage_dir"
