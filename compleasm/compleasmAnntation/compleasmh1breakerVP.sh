596bcd2d4bb35e10e4c4f384e3aab7df57a7cdac
RickZ42 <u7457512@anu.edu.au>
2024-07-08 11:16:53 +1000

    after conference

---
+++ compleasm/compleasmAnntation/compleasmh1breakerVP.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#PBS -P xf3
+#PBS -q normal
+#PBS -l walltime=4:00:00
+#PBS -l ncpus=24
+#PBS -l mem=96GB
+#PBS -l jobfs=400GB
+#PBS -l storage=scratch/xf3+gdata/xf3+gdata/xe2
+#PBS -l wd
+
+source /g/data/xf3/miniconda/etc/profile.d/conda.sh
+conda activate /g/data/xf3/miniconda/envs/compleasm
+
+
+input_file="/g/data/xf3/zz3507/RawData/OrchidProteins/P.Equestris/PEquestris.faa"
+output_dir="/g/data/xf3/zz3507/RawData/OrchidProteins/P.Equestris/compleasm"
+lineage_dir="/g/data/xf3/zz3507/database/lineage"
+threads=24
+mkdir $output_dir
+
+
+
+compleasm protein -p "$input_file" -o "$output_dir" -l embryophyta_odb10 -t "$threads" -L "$lineage_dir"
